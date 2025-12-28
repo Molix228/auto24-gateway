00950b32b990a557d5e0ef866455afbdccf94e53
molix <alex.mesanya@gmail.com>
2025-12-28 11:53:07 +0100

    fixed strict mode for cookies

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -43,7 +43,8 @@ export class AuthController {
       res.cookie('refreshToken', refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        sameSite: 'none',
+        // maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 
       return { user, accessToken, refreshToken };
